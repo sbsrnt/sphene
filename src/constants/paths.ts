dacb2cd89b11b0b7da109593ae9aa3403a8266f0
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-09 14:34:43 +0200

    feat(paths): rename routes to paths and add auth & feature paths

---
+++ src/constants/paths.ts
@@ -0,0 +1,20 @@
+export const PATHS = {
+  /**
+   * Features
+   */
+  HOME: '/',
+  INVOICES: '/invoices',
+  REMINDERS: '/reminders',
+
+  /**
+   * Auth
+   */
+  LOGIN: '/login',
+  FORGOT_PASSWORD: '/forgot-password',
+  RESET_PASSWORD: '/reset-password',
+  REGISTER: '/register',
+  VERIFY_EMAIL: '/verify/:token',
+  RESET_PASSWORD_TOKEN: '/reset-password/:token',
+};
+
+export default PATHS;
