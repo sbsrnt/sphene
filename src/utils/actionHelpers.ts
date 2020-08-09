b9455010e44ddd0d4b95301f6fabd2f58a1ab8be
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-09 14:37:38 +0200

    feat(utils): add API action binders

--- src/utils/actionHelpers.ts
+++ src/utils/actionHelpers.ts
@@ -1,3 +1,34 @@
 type ActionType = string;
 export const success = (actionType: ActionType) => `${actionType}_SUCCESS`;
 export const failure = (actionType: ActionType) => `${actionType}_FAIL`;
+
+type NestError = {
+  error: string;
+  message: string;
+  statusCode: number;
+};
+
+type AxiosError = {
+  data: NestError;
+  status: number;
+};
+
+export const apiErrorBinder = (e?: AxiosError): { error: NestError | null } => {
+  if (!e) return { error: null };
+
+  const { data: error } = e;
+
+  return {
+    error,
+  };
+};
+
+type NestPayload = any;
+
+export const apiSuccessBinder = (payload?: NestPayload): NestPayload => {
+  if (!payload) return {};
+
+  const { data } = payload;
+
+  return data;
+};
