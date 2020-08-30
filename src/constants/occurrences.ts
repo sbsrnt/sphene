975ad95b1f57777bcfe9b0e714c8dafccdafd9a5
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-30 15:45:12 +0200

    feat(reminders): add occurrences const

---
+++ src/constants/occurrences.ts
@@ -0,0 +1,34 @@
+export const occurrences = [
+  {
+    label: 'Daily',
+    value: 0,
+  },
+  {
+    label: 'Every Other Day',
+    value: 1,
+  },
+  {
+    label: 'Weekly',
+    value: 2,
+  },
+  {
+    label: 'Bi-Weekly',
+    value: 3,
+  },
+  {
+    label: 'Monthly',
+    value: 4,
+  },
+  {
+    label: 'Quarterly',
+    value: 5,
+  },
+  {
+    label: 'Half-Yearly',
+    value: 6,
+  },
+  {
+    label: 'Yearly',
+    value: 7,
+  },
+];
