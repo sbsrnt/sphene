a847ec1abb00a34e8d469f00330c16f16f80bddb
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-30 15:46:17 +0200

    feat(formfield): add animation and disabled states

--- src/components/FormField/index.tsx
+++ src/components/FormField/index.tsx
@@ -2,17 +2,21 @@ import React, { ElementType } from 'react';
 import startCase from 'lodash/startCase';
 import styled from 'styled-components';
 
-import { Label, Sublabel } from 'components';
+import { AnimatedList, Label, Sublabel } from 'components';
 
 type FormField = {
   component: ElementType;
   name: string;
   errors: any;
   formRef?: any;
+  control?: any;
   placeholder?: string;
   required?: boolean;
   type?: string;
   dataId?: string;
+  options?: any[];
+  animated?: boolean;
+  disabled?: boolean;
 };
 
 const Field = styled.div`
@@ -25,23 +29,43 @@ const Field = styled.div`
   }
 `;
 
+const StyledItem = styled(AnimatedList.Item)<any>`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.5em;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
 const FormField = ({
   component: T,
   name,
+  control,
   errors,
   formRef,
   required,
   dataId,
+  animated = false,
   ...props
 }: FormField) => {
+  const Tag = animated ? StyledItem : Field;
   return (
-    <Field>
+    <Tag animation="fromTop">
       <Label htmlFor={name} data-cy={`label-${dataId}`}>
         {startCase(name)} {required && '*'}
       </Label>
-      <T id={name} name={name} formRef={formRef} data-cy={`input-${dataId}`} {...props} />
+      <T
+        id={name}
+        name={name}
+        formRef={formRef}
+        control={control}
+        data-cy={`input-${dataId}`}
+        {...props}
+      />
       <Sublabel data-cy={`sublabel-${dataId}`}>{errors[name] && errors[name].message}</Sublabel>
-    </Field>
+    </Tag>
   );
 };
 
