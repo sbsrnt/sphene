67841c5e84179778613648f148e3ebd213b95c59
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-30 15:32:53 +0200

    refactor(input): add font family

--- src/components/inputs/Input/index.tsx
+++ src/components/inputs/Input/index.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components';
 const StyledInput = styled.input`
   border: 0;
   outline: none;
-  padding: 5px;
+  padding: 5px 0;
   background: none;
   border-bottom: 1px solid ${(props) => props.theme.colors.gray100};
   color: ${(props) => props.theme.colors.gray100};
   font-size: ${(props) => props.theme.typography.fontSizes.default};
+  font-family: 'Roboto', sans-serif;
 `;
 
 type InputProps = {
