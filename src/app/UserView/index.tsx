bc0db41681135a1c073b29920cfc4eeae39f5f02
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-30 15:20:02 +0200

    fix(routes): fix guest redirect when accessing user routes

--- src/app/UserView/index.tsx
+++ src/app/UserView/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
 import PATHS from 'constants/paths';
+import { useAuth } from 'hooks';
 import { NavSidebar, UserRoute } from 'components';
 import { Dashboard, Invoicing, Reminders } from 'features';
 
@@ -13,18 +14,27 @@ const Container = styled.div`
   grid-template-columns: 200px calc(100vw - 200px);
 `;
 
-const UserView = () => (
-  <>
-    <Logout />
-    <Container>
-      <NavSidebar />
-      <Switch>
-        <UserRoute exact path={PATHS.DASHBOARD} component={Dashboard} />
-        <UserRoute exact path={PATHS.INVOICES} component={Invoicing} />
-        <UserRoute exact path={PATHS.REMINDERS} component={Reminders} />
-      </Switch>
-    </Container>
-  </>
-);
+const UserView = () => {
+  const { isAuthenticated } = useAuth();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!isAuthenticated) history.push(PATHS.SIGN_IN);
+  }, [isAuthenticated]);
+
+  return !isAuthenticated ? null : (
+    <>
+      <Logout />
+      <Container>
+        <NavSidebar />
+        <Switch>
+          <UserRoute exact path={PATHS.DASHBOARD} component={Dashboard} />
+          <UserRoute exact path={PATHS.INVOICES} component={Invoicing} />
+          <UserRoute exact path={PATHS.REMINDERS} component={Reminders} />
+        </Switch>
+      </Container>
+    </>
+  );
+};
 
 export default UserView;
