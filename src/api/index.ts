17f06bcba9c87dfacdb7a08bbc00ca13047adaf5
Sebastian Krzyżanowski <eskrzy@gmail.com>
2020-08-30 15:18:57 +0200

    feat(api): add reminder endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,15 @@
 const DOMAIN = `http://localhost:4000`;
-
+/**
+ * Auth
+ */
 export const getSignIn = () => `${DOMAIN}/auth/login`;
 export const getSignUp = () => `${DOMAIN}/auth/register`;
 export const getForgotPassword = () => `${DOMAIN}/auth/forgot-password`;
 export const getResetPassword = (token: string) => `${DOMAIN}/auth/reset-password/${token}`;
 export const getVerifyEmail = (token: string | {}) => `${DOMAIN}/auth/verify/${token}`;
+
+/**
+ * Reminders
+ */
+export const allReminders = () => `${DOMAIN}/reminders`;
+export const getReminder = (reminderId: string) => `${DOMAIN}/reminders/${reminderId}`;
